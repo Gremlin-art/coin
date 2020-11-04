7018e6644abacf511855bc4afed75c89fedde991
Gremlin-art <73949322+Gremlin-art@users.noreply.github.com>
2020-11-04 18:27:22 +0300

    Add files via upload

---
+++ precache-manifest.e0ab2ffcf9ad682dfb63970553cd5adb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "3aff1cb66fb54a59b921",
+    "url": "./static/css/main.182cf41f.chunk.css"
+  },
+  {
+    "revision": "3aff1cb66fb54a59b921",
+    "url": "./static/js/main.ec0001ba.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "dd2be6ea43045c65739e",
+    "url": "./static/css/2.43c8f07d.chunk.css"
+  },
+  {
+    "revision": "dd2be6ea43045c65739e",
+    "url": "./static/js/2.ab008e0d.chunk.js"
+  },
+  {
+    "revision": "c53e482c55280e18695f533a6fe47bee",
+    "url": "./static/media/coin.c53e482c.png"
+  },
+  {
+    "revision": "1808b5f6b2dd330757f13c6dc3ddf32a",
+    "url": "./static/media/1233.1808b5f6.ttf"
+  },
+  {
+    "revision": "d8dde2c45fabb1c7700b35203f77be7a",
+    "url": "./static/media/click.d8dde2c4.png"
+  },
+  {
+    "revision": "6a46e43393e7775f685bb762108bc2fb",
+    "url": "./index.html"
+  }
+];
